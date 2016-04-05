1ba1af8639a1261b710d326d484b53f8e026dd63
Eric Lifka <eric.lifka@gmail.com>
2016-04-05 14:53:26 -0400

    add component to get started with addon style

---
+++ addon/components/loan-lifetime-impact/component.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: [ 'loan-lifetime-impact' ]
+});
---
+++ app/components/loan-lifetime-impact/component.js
@@ -0,0 +1,2 @@
+import Component from 'ember-cli-loan-lifetime-impact/components/loan-lifetime-impact/component';
+export default Component;
