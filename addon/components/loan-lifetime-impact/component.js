72195f4a499a065f7e2a3975c6cc0207d8f01c68
Eric Lifka <eric.lifka@gmail.com>
2016-04-07 14:55:47 -0400

    begin calculation by running the next month's payment

--- addon/components/loan-lifetime-impact/component.js
+++ addon/components/loan-lifetime-impact/component.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 import layout from './template';
+let { Component, observer } = Ember;
 
-export default Ember.Component.extend({
+export default Component.extend({
   classNames: [ 'loan-lifetime-impact' ],
   layout,
 
   loanAmount: 0,
   interestRate: 0,
-  monthlyPayment: 0
+  monthlyPayment: 0,
+
+  runCalculation: observer('loanAmount', 'interestRate', 'monthlyPayment', function () {
+    let interestDecimal = this.get('interestRate') / 100.0 / 12;
+    let nextMonthInterest = this.get('loanAmount') * interestDecimal;
+    let nextMonthPrincipal = this.get('monthlyPayment') - nextMonthInterest;
+
+    this.set('nextMonthInterest', nextMonthInterest);
+    this.set('nextMonthPrincipal', nextMonthPrincipal);
+  })
 });
