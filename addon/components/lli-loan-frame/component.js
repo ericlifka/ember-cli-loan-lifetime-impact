e23858e2f49eebbe1f47ac7520c3b3d2be436967
Eric Lifka <eric.lifka@gmail.com>
2016-04-07 20:38:50 -0400

    add a component to render the frames

---
+++ addon/components/lli-loan-frame/component.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import layout from './template';
+let { Component, computed, observer } = Ember;
+
+export default Component.extend({
+  classNames: [ 'lli-loan-frame' ],
+  layout
+});
---
+++ app/components/lli-loan-frame/component.js
@@ -0,0 +1,2 @@
+import Component from 'ember-cli-loan-lifetime-impact/components/lli-loan-frame/component';
+export default Component;
