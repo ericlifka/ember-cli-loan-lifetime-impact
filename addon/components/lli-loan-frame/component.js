7f3bf27da278b871151ab15816550ca378e22c3b
Eric Lifka <eric.lifka@gmail.com>
2016-04-08 09:44:42 -0400

    fix display styles so they show no more than 2 decimal places

--- addon/components/lli-loan-frame/component.js
+++ addon/components/lli-loan-frame/component.js
@@ -6,14 +6,22 @@ export default Component.extend({
   classNames: [ 'lli-loan-frame' ],
   layout,
 
+  loanTotalDisplay: computed('frame.endingBalance', function () {
+    return this.truncTwoDecimals(this.get('frame.endingBalance'));
+  }),
+
   percentComplete: computed('frame.{loanAmount,endingBalance}', function () {
     let loanAmount = this.get('frame.loanAmount');
     let endingBalance = this.get('frame.endingBalance');
 
-    return `${endingBalance / loanAmount * 100}`.slice(0, 4) + '%';
+    return this.truncTwoDecimals(endingBalance / loanAmount * 100) + '%';
   }),
 
   loanRemainingStyle: computed('percentComplete', function () {
     return `width: ${this.get('percentComplete')}`;
-  })
+  }),
+
+  truncTwoDecimals(amnt) {
+    return Math.round(amnt * 100) / 100;
+  }
 });
